f7add2be9eb0af5c218ccc523fd33353058b9bc5
Ahnaf <ahnafk000@gmail.com>
2024-01-06 14:41:51 +0600

    added a fruit detector

--- pages/Projects.py
+++ pages/Projects.py
@@ -40,7 +40,7 @@ def pet_detector():
 
 @st.cache_resource
 def load_image_captioning_model():
-
+    # source: https://huggingface.co/nlpconnect/vit-gpt2-image-captioning
     image_to_text_model = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
     return image_to_text_model
     
@@ -62,6 +62,32 @@ def image_captioning():
                 st.success(f"Here's what I think: \"{caption}\"")
 
 
+@st.cache_resource
+def load_fruit_detector_model():
+    model = pipeline("image-classification", model="jazzmacedo/fruits-and-vegetables-detector-36")
+    return model
+
+def fruit_detector():
+    st.write("## Fruit Fiasco Finder")
+    st.write("Hey there! I'm the Fruit Fiasco Finder. Pop in a photo and I'll sniff out the fruits in the bunch!")
+
+    uploaded_file = st.file_uploader("Drop a fruit image here and watch me go bananas!", type=["jpg", "png", "jpeg"], key="fruit_detector_uploader")
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Let's take a peek at this fruity puzzle...", use_column_width=True)
+
+        if st.button("Identify the Fruit"):
+            with st.spinner("Scrutinizing the scrumptious..."):
+                model = load_fruit_detector_model()
+                results = model(image)
+                if results[0]['score'] >= 0.999:
+                    st.success(f"Confidently, that's a {results[0]['label']}!")
+                else:
+                    descriptions = [f"{res['label']} ({res['score'] * 100:.2f}%)" for res in results[:5]]
+                    st.success(f"I see: {', '.join(descriptions)}")
+
+
+
 
 # Function to display the Projects page
 def render_projects():
@@ -75,6 +101,10 @@ def render_projects():
     # Use Streamlit expanders for each project
     with st.expander("Image Captioning Project", expanded=False):
         image_captioning()  # The model will load when this expander is opened
+
+    # Use Streamlit expanders for each project
+    with st.expander("Fruits Identifier Project", expanded=False):
+        fruit_detector()  # The model will load when this expander is opened
 
     # Add more expanders for each additional project you want to include
 
